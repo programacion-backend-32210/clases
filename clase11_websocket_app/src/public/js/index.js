860b7e0767bece1b7f6252c664838462ddc7496e
Arturo Verbel <arturo.verbel@bia.app>
2022-12-22 18:02:08 -0500

    Clase 11 -  Websocket app

---
+++ clase11_websocket_app/src/public/js/index.js
@@ -0,0 +1,43 @@
+let socket = io()
+let user = ''
+let chatBox = document.getElementById('chatbox')
+
+Swal.fire({
+    title: 'Authentication',
+    input: 'text',
+    text: 'Set username for the Bianca\'s chat',
+    inputValidator: value => {
+        return !value.trim() && 'Please. Write a username!'
+    },
+    allowOutsideClick: false
+}).then( result => {
+    user = result.value
+    document.getElementById('username').innerHTML = user
+})
+
+// Enviamos mensajes
+chatBox.addEventListener('keyup', event => {
+    if(event.key === 'Enter') {
+        if(chatBox.value.trim().length > 0) {
+            socket.emit('message', {
+                user,
+                message: chatBox.value
+            })
+
+            chatBox.value = ''
+        }
+    }
+})
+
+
+// Recibir Mensahes
+socket.on('logs', data => {
+    const divLog = document.getElementById('messageLogs')
+    let messages = ''
+
+    data.reverse().forEach(message => {
+        messages += `<p><i>${message.user}</i>: ${message.message}</p>`
+    });
+
+    divLog.innerHTML = messages
+})
