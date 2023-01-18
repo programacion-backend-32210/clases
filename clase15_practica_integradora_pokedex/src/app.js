b57851abe73f920e3ed270852bc869fc3c08871a
Arturo Verbel <arturo.verbel@bia.app>
2023-01-18 18:09:13 -0500

    Clase 15 - Practica Integradora Pokedex (Parte 2)

--- clase15_practica_integradora_pokedex/src/app.js
+++ clase15_practica_integradora_pokedex/src/app.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import express from 'express'
 import handlebars from 'express-handlebars'
 import pokeRouter from './routes/pokedex.router.js'
@@ -6,19 +7,35 @@ import __dirname from './utils.js'
 
 const app = express()
 
+// Para traer info de post como JSON
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 //Configurar el motor de plantillas
 app.engine('handlebars', handlebars.engine())
 app.set('views', __dirname + '/views')
 app.set('view engine', 'handlebars')
 
 // Carpeta publica
-app.use(express.static( __dirname + '/public'))
+app.use(express.static(__dirname + '/public'))
 
 // Congfiguracion de rutas
 app.use('/pokedex', pokeRouter)
 app.use('/trainer', trainerRouter)
 app.get('/', (req, res) => res.send('Wrok great!'))
 
-// Corriendo el servidor
-const server = app.listen(8080, () => console.log('Server listening...'))
-server.on('error', e => console.log(e))
+const uri = "mongodb+srv://r2d2:TImCZj6dj0XAaHFN@cluster0.gl63h07.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.set('strictQuery', false)
+mongoose.connect(uri, error => {
+    if (error) {
+        console.log('No se pudo conectar a la DB');
+        return
+    }
+    console.log('DB connected!');
+
+    // Corriendo el servidor
+    const server = app.listen(8080, () => console.log('Server listening...'))
+    server.on('error', e => console.log(e))
+
+})
--- clase15_practica_integradora_pokedex/src/routes/pokedex.router.js
+++ clase15_practica_integradora_pokedex/src/routes/pokedex.router.js
@@ -1,28 +1,47 @@
 import { Router } from 'express'
+import pokeModel from '../models/pokemon.models.js'
 
 const router = Router()
 
 // Listar pokemons
-router.get('/', (req, res) => {
-    res.render('list', {})
+router.get('/', async (req, res) => {
+    const pokemons = await pokeModel.find().lean().exec()
+    
+    res.render('list', { pokemons })
 })
+
+// Pagina para crear pokemons
+router.get('/create', (req, res) => {
+    res.render('create', {})
+})
+
+
 
 // Obtener un pokemon
-router.get('/:name', (req, res) => {
+router.get('/:name', async (req, res) => {
     const name = req.params.name
 
-    res.render('one', {
-        name
-    })
+    const pokemon = await pokeModel.findOne({name: name}).lean().exec()
+    
+    res.render('one', { pokemon })
 })
 
+
 // Crear pokemons
-router.post('/', (req, res) => {
-    res.send('Creando pokemon')
+router.post('/', async (req, res) => {
+    const pokemonNew = req.body
+    console.log(pokemonNew);
+
+    const pokemonGenerated = new pokeModel(pokemonNew)
+    await pokemonGenerated.save()
+
+    console.log(pokemonGenerated);
+
+    res.redirect('/pokedex/' + pokemonGenerated.name)
 })
 
 // BOrrar un pokemon
-router.delete('/:id', (req, res) => {
+router.get('/delete/:name', (req, res) => {
     res.send('Borrando pokemon')
 })
 
