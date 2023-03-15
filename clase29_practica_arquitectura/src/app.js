05dbcb35ab00e172dd2b9c785d610798a879d52b
Arturo Verbel <arturo-verbel@hotmail.com>
2023-03-14 19:00:25 -0500

    Clase 29 - Practica Arquitectura (4)

--- clase29_practica_arquitectura/src/app.js
+++ clase29_practica_arquitectura/src/app.js
@@ -3,11 +3,16 @@ import usersRouter from './routes/users.router.js'
 import ordersRouter from './routes/orders.router.js'
 import storesRouter from './routes/stores.router.js'
 import mongoose from 'mongoose'
+import cors from 'cors'
 
 const PORT = process.env.PORT || 8080
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
+app.use(cors({
+    origin: 'http://127.0.0.1:5500',
+    methods: ['GET', "POST", 'PUT']
+}))
 
 app.use('/api/users', usersRouter)
 app.use('/api/orders', ordersRouter)
---
+++ clase29_practica_arquitectura_front/index.js
@@ -0,0 +1,31 @@
+
+fetch('http://127.0.0.1:8080/api/orders')
+    .then(result => result.json())
+    .then(json => {
+
+        console.log(json)
+
+        const fragment = document.createDocumentFragment()
+
+        json.result.forEach(order => {
+            const div = document.createElement('div')
+            const priceParagraph = document.createElement('p')
+            const statusParagraph = document.createElement('p')
+            const numberParagraph = document.createElement('p')
+
+            priceParagraph.innerHTML = `total de la ordern: ${order.totalPrice}`
+            statusParagraph.innerHTML = `Status: ${order.status}`
+            numberParagraph.innerHTML = `Order Number: ${order.number}`
+
+            div.appendChild(numberParagraph)
+            div.appendChild(priceParagraph)
+            div.appendChild(statusParagraph)
+
+            fragment.appendChild(div)
+            
+        });
+
+        const orderContainer = document.getElementById('orders')
+        orderContainer.appendChild(fragment)
+
+    })
